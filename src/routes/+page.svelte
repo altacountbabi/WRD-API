5b7b795f2479f6211fd84fa0e8a889f563bde420
altacountbabi <radustefan379@gmail.com>
2023-07-27 17:23:16 +0300

    Poppins looks ass for main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
             width: 100%;
             height: 100%;
             overflow: hidden;
-            font-family: poppins,Poppins,Arial,sans-serif;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
             scroll-behavior: smooth;
         }
     </style>
