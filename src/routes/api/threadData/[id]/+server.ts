8932719a7b9257b31c06c8aa156930a50e32ac37
Whoman <82091823+altacountbabi@users.noreply.github.com>
2023-07-31 16:05:47 +0300

    Update +server.ts

--- src/routes/api/threadData/[id]/+server.ts
+++ src/routes/api/threadData/[id]/+server.ts
@@ -5,7 +5,7 @@ export async function GET({ request }: any) {
 	let id: string = request.url
 		.match(/\/[0-9a-zA-Z]+/g)
 		?.join("")
-		.replace("wrd-api.vercel.app/api/threadData/", "") as string
+		.replace("/wrd/api/threadData/", "") as string
 	const result = await api.fetchThreadData(id)
 
 	if (result.author.uid == "") {
